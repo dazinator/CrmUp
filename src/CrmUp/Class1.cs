ff5103fdd8227ab1da57ed2d5dd7aa759a7efe4d
Darrell <darrell.tunnell@googlemail.com>
2014-01-12 15:53:24 +0000

    Initial solution structure created.

---
+++ src/CrmUp/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using DbUp;
+
+namespace CrmUp
+{
+    public class Class1
+    {
+
+        static int Main(string[] args)
+        {
+            var connectionString = args.FirstOrDefault()
+                ?? "Server=(local)\\SqlExpress; Database=MyApp; Trusted_connection=true";
+
+            var upgrader =
+                DeployChanges.To
+                    .SqlDatabase(connectionString)
+                    .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                    .LogToConsole()
+                    .Build();
+
+            var result = upgrader.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();
+                return -1;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success!");
+            Console.ResetColor();
+            return 0;
+        }
+
+    }
+}
