6df8aabc469d9aa0310dc0e0dbb2807ae3e198a4
Darrell <darrell.tunnell@googlemail.com>
2014-01-17 00:22:06 +0000

    Added console app

---
+++ src/CrmUp.Console/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using DbUp;
+
+namespace CrmUp
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+
+            var connectionString = args.FirstOrDefault();
+            bool isKey = false;
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                isKey = true;
+                connectionString = "CrmConnectionString";
+            }
+         
+            var upgrader =
+               DeployChanges.To
+                            .DynamicsCrm(connectionString, isKey)
+                            .WithSolutionsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                            .LogToConsole()
+                            .Build();
+
+            var result = upgrader.PerformUpgrade();
+
+            if (!result.Successful)
+            {
+                
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(result.Error);
+                Console.ResetColor();
+                return -1;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Success!");
+            Console.ResetColor();
+            return 0;
+
+        }
+    }
+}
