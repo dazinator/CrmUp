90238fc00c59b904ab42e3a0228140765ba2f3c6
Darrell Tunnell <darrell.tunnell@googlemail.com>
2014-01-24 11:46:19 +0000

    Release build now publishes to NuGet as long as SetAPIKey has been run approriately on machine.

---
+++ src/Information/SolutionInfo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("DbUp Contributors")]
+[assembly: AssemblyProduct("DbUp")]
+[assembly: AssemblyCopyright("Copyright © DbUp Contributors 2010")]
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
