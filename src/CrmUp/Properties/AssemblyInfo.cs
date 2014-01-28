ac41acc590cf100d4733b5bd9f27be378e073063
Darrell Tunnell <darrell.tunnell@googlemail.com>
2014-01-28 12:32:39 +0000

    incremented minor version number as code based migrations now supported.

--- src/CrmUp/Properties/AssemblyInfo.cs
+++ src/CrmUp/Properties/AssemblyInfo.cs
@@ -6,11 +6,13 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CrmUp")]
-[assembly: AssemblyDescription("CrmUp")]
+[assembly: AssemblyDescription("CrmUp is a .NET library that helps you to automate the deployment of your Microsoft Dynamics Crm customisations. See http://dazinator.github.io/CrmUp/ for details.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("31aa40c5-490f-4ea5-8f76-bfec93c75191")]
-[assembly: AssemblyVersion("1.0.2.*")]
-[assembly: AssemblyFileVersion("1.0.2.0")]
+[assembly: AssemblyVersion("1.0.3.*")]
+[assembly: AssemblyFileVersion("1.0.3.0")]
+[assembly: AssemblyCompanyAttribute("Darrell Tunnell")]
+[assembly: AssemblyProductAttribute("CrmUp")]
